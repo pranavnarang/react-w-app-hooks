17bc09cbbf210ce806494f3f2db66d2ec85f2866
Pranav Narang <pnarang1130@gmail.com>
2020-08-31 16:34:18 -0600

    updated main to use env variables

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import Context from "../Context"
 
@@ -11,7 +11,7 @@ import Error from "./Error"
 import DateTime from "./DateTime"
 import Footer from "./Footer"
 
-const API_KEY = '3ae01fa0fbc8643e12a00c56cf570895';
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 
 const Main = () => {
@@ -23,7 +23,7 @@ const Main = () => {
     const api_call = async e => {
         e.preventDefault()
         const location = e.target.elements.location.value
-        if(!location) return setError("Please enter the name of the city"), setWeather(null)
+        if (!location) return setError("Please enter the name of the city"), setWeather(null)
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`
         const request = axios.get(url)
         const response = await request
@@ -35,17 +35,17 @@ const Main = () => {
     return (
 
         <div className="main">
-           <Header />
-           <Content>
+            <Header />
+            <Content>
                 <DateTime />
                 <TagLine />
-                <Context.Provider value={{ api_call , weather, city}}>
+                <Context.Provider value={{ api_call, weather, city }}>
                     <WeatherSearch />
-                    { weather && <WeatherData /> }
-                    { error && <Error error={error} /> }
+                    {weather && <WeatherData />}
+                    {error && <Error error={error} />}
                 </Context.Provider>
                 <Footer />
-           </Content>
+            </Content>
         </div>
     )
 }
